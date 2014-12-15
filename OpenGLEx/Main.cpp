20c2751e8c04080af175d0df0e7036346010fc70
Salihan04 <salihan04@gmail.com>
2014-12-15 20:36:00 +0800

    Draw lines and draw random lines using OpenGL

--- OpenGLEx/Main.cpp
+++ OpenGLEx/Main.cpp
@@ -6,39 +6,94 @@
 
 using namespace std;
 
+//function to return a random float type random number
 float random() {
 	return (float)(rand() % 1000 - 500) / 500;
 }
 
-//Function to draw some points
+//function to draw some points
 void drawPoints() {
-	//Set color of points in RGB format, default color is white
+	//set color of points in RGB format, default color is white
 	glColor3f(0.0f, 1.0f, 0.0f);	//Green
 
-	//Set size of points
+	//set size of points
 	glPointSize(10.0f);
 
-	//Set coordinates of points
+	//draw points
 	glBegin(GL_POINTS);
-	glVertex2f(0.0f, 0.0f);
-	glVertex2f(0.1f, 0.1f);
-	glVertex2f(0.2f, 0.2f);
-	glVertex2f(0.3f, 0.3f);
-	glVertex2f(0.4f, 0.4f);
+		glVertex2f(0.0f, 0.0f);
+		glVertex2f(0.1f, 0.1f);
+		glVertex2f(0.2f, 0.2f);
+		glVertex2f(0.3f, 0.3f);
+		glVertex2f(0.4f, 0.4f);
 	glEnd();
 }
 
-//Function to draw points at random positions with random colors
+//function to draw points at random positions with random colors
 void drawRandomPoints() {
-	//Set color of points in RGB format, default color is white
+	//set color of points in RGB format, default color is white
 	glColor3f(random(), random(), random());
 
-	//Set size of points
+	//set size of points
 	glPointSize(10.0f);
 
-	//Set coordinates of points
+	//draw points
 	glBegin(GL_POINTS);
 	for (int i = 0; i < 100; i++) {
+		glVertex2f(random(), random());
+	}
+	glEnd();
+}
+
+//function to draw some lines
+void drawLines() {
+	//set color of points in RGB format, default color is white
+	glColor3f(0.0f, 1.0f, 0.0f);	//Green
+
+	//set size of points
+	glPointSize(10.0f);
+
+	//draw points
+	glBegin(GL_POINTS);
+	glVertex2f(0.5f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(-0.5f, 0.0f);
+		glVertex2f(0.0f, -0.5f);
+		glVertex2f(0.5f, 0.0f);
+	glEnd();
+
+	//set color of points in RGB format, default color is white
+	glColor3f(1.0f, 0.0f, 0.0f);	//Red
+
+	//set line thickness
+	glLineWidth(5.0f);
+
+	//draw lines
+	//each pair of points form a line
+	glBegin(GL_LINES);
+		glVertex2f(0.5f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(-0.5f, 0.0f);
+	
+		glVertex2f(-0.5f, 0.0f);
+		glVertex2f(0.0f, -0.5f);
+	
+		glVertex2f(0.0f, -0.5f);
+		glVertex2f(0.5f, 0.0f);
+	glEnd();
+}
+
+void drawRandomLines() {
+	//set color of points in RGB format, default color is white
+	glColor3f(random(), random(), random());
+
+	//draw lines
+	//each pair of points form a line
+	glBegin(GL_LINES);
+	for (int i = 0; i < 100; i++) {
+		glVertex2f(random(), random());
 		glVertex2f(random(), random());
 	}
 	glEnd();
@@ -47,10 +102,12 @@ void drawRandomPoints() {
 void renderScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//drawPoints();
-	drawRandomPoints();
+//	drawPoints();
+//	drawRandomPoints();
+//	drawLines();
+	drawRandomLines();
 
-	glutSwapBuffers();
+	glutSwapBuffers();	//to prevent flickering when rendering scene
 	glutPostRedisplay();
 }
 
